a3a526835e06ef374d5dd9ab25620245f773bcad
gsilvapt <gustavosantaremsilva@gmail.com>
2018-02-20 14:06:25 +0000

    Adds timestamps to messages.

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message:', message);
+  let formattedTime = moment(message.createdAt).format('hh:mm a');
   let li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`(${formattedTime}) ${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+  let formattedTime = moment(message.createdAt).format('hh:mm a')
   let li = jQuery('<li></li>');
   let a = jQuery('<a target="_blank">My Current Location</a>')
 
-  li.text(`${message.from}: `);
+  li.text(` (${formattedTime}) ${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
 
