a5400b8161ac9120cc770be29b82e839f08a5148
gsilvapt <gustavosantaremsilva@gmail.com>
2018-02-21 12:10:03 +0000

    Add mustache and message templating.

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,29 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('hh:mm a');
-  let li = jQuery('<li></li>');
-  li.text(`(${formattedTime}) ${message.from}: ${message.text}`);
+  let template = jQuery('#message-template').html();
+  let html = Mustache.render(template, {
+    createdAt: formattedTime,
+    from: message.from,
+    text: message.text
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+
 });
 
 socket.on('newLocationMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('hh:mm a')
-  let li = jQuery('<li></li>');
-  let a = jQuery('<a target="_blank">My Current Location</a>')
+  let template = jQuery('#location-message-template').html();
+  let html = Mustache.render(template, {
+    createdAt: formattedTime,
+    from: message.from,
+    url: message.url
+  });
 
-  li.text(` (${formattedTime}) ${message.from}: `);
-  a.attr('href', message.url);
-  li.append(a);
+  jQuery('#messages').append(html);
 
-  jQuery('#messages').append(li);
-})
+});
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
